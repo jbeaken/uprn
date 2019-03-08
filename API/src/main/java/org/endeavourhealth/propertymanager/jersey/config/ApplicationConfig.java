752c6682322382fe35eefa3527a954f4bf899878
hal <hal@hal.com>
2019-03-08 15:24:29 +0000

    Adding hibernate to managed jersey resource to allow for @injet

--- API/src/main/java/org/endeavourhealth/propertymanager/jersey/config/ApplicationConfig.java
+++ API/src/main/java/org/endeavourhealth/propertymanager/jersey/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package org.endeavourhealth.propertymanager.jersey.config;
 
+import org.endeavourhealth.skeleton.api.dal.TemplateDAL_Hibernate;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ApplicationConfig extends ResourceConfig {
     public ApplicationConfig() {
         register(new ApplicationBinder());
+        register(new TemplateDAL_Hibernate());
     }
 }
--- Database/import/import-discovery-addresses.sql
+++ Database/import/import-discovery-addresses.sql
@@ -1,23 +1,24 @@
+-- "OrgPostcode","PersonPseudoId","AddressLine1","AddressLine2","AddressLine3","AddressLine4","County","Postcode"
+
 drop table uprn.address;
 
 create table uprn.address (
-     originalPostcode VARCHAR(255),
-     id BIGINT PRIMARY KEY,
+     originalPostcode VARCHAR(35),
+     id BIGINT,
      line1 VARCHAR(255),
      line2 VARCHAR(255),
      line3 VARCHAR(255),
      line4 VARCHAR(255),
-     county VARCHAR(255),
-     postcode  VARCHAR(255),
-     onsAddress VARCHAR(255),
-     score INT
+     county VARCHAR(35),
+     postcode VARCHAR(35)
 );
 
-
-
-LOAD DATA LOCAL INFILE '/media/ext/LearningHealth/address_extract.csv'
+LOAD DATA LOCAL INFILE '/media/ext/LearningHealth/address_extract2.csv'
   INTO TABLE uprn.address
   FIELDS TERMINATED BY ','
   ENCLOSED BY '"'
   LINES TERMINATED BY '\n'
   IGNORE 1 ROWS;
+
+alter table address add column onsAddress VARCHAR(255);
+alter table address add column score INT;
