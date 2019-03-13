f113a3d94646b048dd3b90bdded5a722073c65b3
hal <hal@hal.com>
2019-03-13 15:45:21 +0000

    Added support for processing mumps matched and unmatched mumps csvs, using directories batch/input batch/output

--- batch/src/main/java/org/endeavourhealth/propertymatcher/PropertymatcherApplication.java
+++ batch/src/main/java/org/endeavourhealth/propertymatcher/PropertymatcherApplication.java
@@ -25,92 +25,124 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootApplication
 public class PropertymatcherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PropertymatcherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PropertymatcherApplication.class, args);
+    }
 }
 
 @Component
 final class CommandLineAppStartupRunner implements CommandLineRunner {
 
-	private final Logger logger = LoggerFactory.getLogger(CommandLineAppStartupRunner.class);
+    private final Logger logger = LoggerFactory.getLogger(CommandLineAppStartupRunner.class);
 
-	private final String url = "http://localhost:9001/addresses?verbose=true&matchthreshold=5&rangekm=&historical=true&offset=0&classificationfilter=&lon=-3.5091076&enddate=&limit=10&input=";
+    private final String url = "http://localhost:9001/addresses?verbose=true&matchthreshold=5&rangekm=&historical=true&offset=0&classificationfilter=&lon=-3.5091076&enddate=&limit=10&input=";
 
-	private final RestTemplate restTemplate = new RestTemplateBuilder().build();
+    private final RestTemplate restTemplate = new RestTemplateBuilder().build();
 
-	private CSVPrinter csvPrinter;
+    private CSVPrinter csvPrinter;
 
-	private final ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
-	final int counterStart = 174900;
-	final int counterEnd = 2_000_000;
+    private String csvInput;
+    private String csvOutput;
 
-    private final String outputCSVFilename = "/media/ext/LearningHealth/output" + counterStart + ".csv";
+    final int counterStart = 0;
+    final int counterEnd = 100_000_000;
+    final long start = System.currentTimeMillis();
+
+    public void run(String... args) throws Exception {
+
+        logger.info("Import of csv started with command-line arguments: {} .", Arrays.toString(args));
+
+        String csvType = args[0];
+
+        initialise(csvType);
+
+        initialiseCSVPrinter();
+
+        processRecords(csvType);
+
+        long end = System.currentTimeMillis();
+
+        logger.info("Finished!! Have processed {} records in {} milliseconds", (counterEnd - counterStart), (end - start));
+
+        logger.info("Start position {} to end {}", counterStart, counterEnd);
+
+        csvPrinter.flush();
+        csvPrinter.close();
+    }
 
 
-	public void run(String... args) throws Exception {
+    private void initialise(String csvType) {
+        if(csvType.equals("unmatched")) {
+            csvInput = "/media/ext/LearningHealth/batch/input/unmatched.csv";
+            csvOutput = "/media/ext/LearningHealth/batch/output/unmatched.csv";
+        } else if(csvType.equals("unmatched")) {
+            csvInput = "/media/ext/LearningHealth/batch/input/discovery.csv";
+            csvOutput = "/media/ext/LearningHealth/batch/output/discovery" + start + ".csv";
+        } else throw new RuntimeException(csvType + " csvType not supported!");
+    }
 
-		logger.info("Import of csv started with command-line arguments: {} .", Arrays.toString(args));
+    private void processRecords(String csvType) throws IOException {
 
-		long start = System.currentTimeMillis();
-		int counter = 0;
+        logger.info("Processing records for type {}", csvType);
 
-		initialiseCSVPrinter();
+        int counter = 0;
 
-		for (CSVRecord record : readCSVRecords()) {
+        Iterable<CSVRecord> records = readCSVRecords();
+
+        for (CSVRecord record : records) {
 
             counter++;
 
-		    if(counter < counterStart) continue;
+            CSVAddress csvAddress = null;
 
-			CSVAddress csvAddress = new CSVAddress(record);
+            if (counter < counterStart) continue;
 
-			String json = getResponseFromOnsServer(csvAddress);
+            if (csvType.equals("unmatched")) {
+                csvAddress = CSVAddress.getUnmatchedCSVAddress(record);
+            } else {
+                csvAddress = CSVAddress.getDiscoveryCSVAddress(record);
+            }
 
-			ONSAddress onsAddress = getOnsAddressFromJson( json );
+            String json = getResponseFromOnsServer(csvAddress);
 
-			printToCsv(csvAddress, onsAddress);
+            ONSAddress onsAddress = getOnsAddressFromJson(json);
 
-			if (counter % 100 == 0) {
-				long end = System.currentTimeMillis();
-				logger.info("Have processed {} records in {} milliseconds", counter, (end - start));
-			}
+            printToCsv(csvAddress, onsAddress);
 
-			if(counter == counterEnd) break;
-		}
+            if (counter % 100 == 0) {
+                long end = System.currentTimeMillis();
+                logger.info("Have processed {} records in {} milliseconds", counter, (end - start));
+            }
 
-		long end = System.currentTimeMillis();
+            if (counter == counterEnd) break;
+        }
+    }
 
-		logger.info("Finished!! Have processed {} records in {} milliseconds", counter, (end - start));
+    private Iterable<CSVRecord> readCSVRecords() throws IOException {
 
-		csvPrinter.flush();
-		csvPrinter.close();
-	}
-
-	private Iterable<CSVRecord> readCSVRecords() throws IOException {
-
-		Reader in = new FileReader("/media/ext/LearningHealth/address_extract.csv");
+        Reader in = new FileReader( csvInput );
 
         return CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
-	}
+    }
 
-	private void initialiseCSVPrinter() throws IOException {
+    private void initialiseCSVPrinter() throws IOException {
 
-		Writer writer = new BufferedWriter(new FileWriter( outputCSVFilename ));
+        Writer writer = new BufferedWriter(new FileWriter( csvOutput ));
 
-		csvPrinter = new CSVPrinter(writer,
-				CSVFormat.DEFAULT.withHeader(
-						"DiscoveryAddress",
-						"Score",
-						"ONSAddress",
-						"UPRN",
-						"Status",
+        csvPrinter = new CSVPrinter(writer,
+                CSVFormat.DEFAULT.withHeader(
+                        "DiscoveryAddress",
+                        "Score",
+                        "ONSAddress",
+                        "UPRN",
+                        "Status",
                         "Classification",
 
-						//Import from discovery csv
+                        //Import from discovery csv
 
-						"Line1",
+                        "Line1",
                         "Line2",
                         "Line3",
                         "Line4",
@@ -118,17 +150,17 @@ final class CommandLineAppStartupRunner implements CommandLineRunner {
                         "Postcode",
                         "OrgPostcode",
                         "PseudoPersonid"));
-	}
+    }
 
-	private ONSAddress getOnsAddressFromJson(String json) throws IOException {
+    private ONSAddress getOnsAddressFromJson(String json) throws IOException {
 
-		JsonNode root = mapper.readTree(json);
+        JsonNode root = mapper.readTree(json);
 
         Iterator<JsonNode> addresses = root.path("response").get("addresses").iterator();
 
-		ONSAddress onsAddress = new ONSAddress();  //No match
+        ONSAddress onsAddress = new ONSAddress();  //No match
 
-		if (addresses.hasNext()) {
+        if (addresses.hasNext()) {
 
             onsAddress = getOnsAddress(addresses.next());
 
@@ -139,21 +171,21 @@ final class CommandLineAppStartupRunner implements CommandLineRunner {
 
             onsAddress.setStatus("MATCH");
 
-		}
+        }
 
-		return onsAddress;
-	}
+        return onsAddress;
+    }
 
-	private ONSAddress getOnsAddress(JsonNode address) {
-		ONSAddress onsAddress = new ONSAddress();
-		
-		onsAddress.setConfidenceScore( address.get("confidenceScore").asDouble() );
-		
-		onsAddress.setFormattedAddress( address.get("formattedAddress").asText() );
-		
-		onsAddress.setUprn( address.get("uprn").asText() );
+    private ONSAddress getOnsAddress(JsonNode address) {
+        ONSAddress onsAddress = new ONSAddress();
 
-		onsAddress.setClassificationCode( address.get("classificationCode").asText() );
+        onsAddress.setConfidenceScore(address.get("confidenceScore").asDouble());
+
+        onsAddress.setFormattedAddress(address.get("formattedAddress").asText());
+
+        onsAddress.setUprn(address.get("uprn").asText());
+
+        onsAddress.setClassificationCode(address.get("classificationCode").asText());
 
 //		JsonNode paf = address.get("paf");
 //
@@ -169,41 +201,43 @@ final class CommandLineAppStartupRunner implements CommandLineRunner {
 //			Nag n = new Nag(node.get("postcodeLocator").asText(), node.get("locality").asText() );
 //			onsAddress.setNag(n);
 //		}
-		
-		return onsAddress;
-	}
 
-	private void printToCsv(CSVAddress csvAddress, ONSAddress address) throws IOException {
+        return onsAddress;
+    }
 
-		csvPrinter.printRecord(
-				csvAddress.getQ(),
+    private void printToCsv(CSVAddress csvAddress, ONSAddress address) throws IOException {
 
-				address.getConfidenceScore(),
+        csvPrinter.printRecord(
+                csvAddress.getQ(),
+
+                address.getConfidenceScore(),
                 address.getFormattedAddress(),
-				address.getUprn(),
-				address.getStatus(),
-   				address.getClassificationCode(),
+                address.getUprn(),
+                address.getStatus(),
+                address.getClassificationCode(),
 
-   				csvAddress.getLine1(),
-				csvAddress.getLine2(),
-				csvAddress.getLine3(),
-				csvAddress.getLine4(),
+                csvAddress.getLine1(),
+                csvAddress.getLine2(),
+                csvAddress.getLine3(),
+                csvAddress.getLine4(),
                 csvAddress.getCounty(),
                 csvAddress.getPostcode(),
                 csvAddress.getOrgPostcode(),
-				csvAddress.getPsuedoPersonId());
+                csvAddress.getPsuedoPersonId());
 
 
     }
 
-	private String getResponseFromOnsServer(CSVAddress csvAddress) {
+    private String getResponseFromOnsServer(CSVAddress csvAddress) {
 
-		String q = csvAddress.getQ();
+        String q = csvAddress.getQ().replace("#", "");
 
-		ResponseEntity<String> stringEntity = restTemplate.getForEntity(url + q, String.class);
+        logger.debug(q);
 
-		return stringEntity.getBody();
+        ResponseEntity<String> stringEntity = restTemplate.getForEntity(url + q, String.class);
 
-	}
+        return stringEntity.getBody();
+
+    }
 
 }
--- batch/src/main/java/org/endeavourhealth/propertymatcher/bean/CSVAddress.java
+++ batch/src/main/java/org/endeavourhealth/propertymatcher/bean/CSVAddress.java
@@ -6,18 +6,32 @@ import lombok.Data;
 
 @Data
 public class CSVAddress {
-	public CSVAddress(CSVRecord record) {
-		this();
-		this.line1 = record.get("AddressLine1");
-		this.line2 = record.get("AddressLine2");
-		this.line3 = record.get("AddressLine3");
-		this.line4 = record.get("AddressLine4");
-		this.county = record.get("County");
-		this.postcode = record.get("Postcode");
-		this.psuedoPersonId = record.get("PersonPseudoId");
-		this.orgPostcode = record.get("OrgPostcode");
 
+	public static CSVAddress getUnmatchedCSVAddress(CSVRecord record) {
+		CSVAddress address = new CSVAddress();
+		address.line1 = record.get("AddressLine1");
+		address.line2 = record.get("AddressLine2");
+		address.line3 = record.get("AddressLine3");
+		address.line4 = record.get("AddressLine4");
+		address.county = record.get("County");
+		address.postcode = record.get("Postcode");
+		address.psuedoPersonId = record.get("PersonPseudoId");
 
+		return address;
+	}
+
+	public static CSVAddress getDiscoveryCSVAddress(CSVRecord record) {
+		CSVAddress address = new CSVAddress();
+		address.line1 = record.get("AddressLine1");
+		address.line2 = record.get("AddressLine2");
+		address.line3 = record.get("AddressLine3");
+		address.line4 = record.get("AddressLine4");
+		address.county = record.get("County");
+		address.postcode = record.get("Postcode");
+		address.psuedoPersonId = record.get("PersonPseudoId");
+		address.orgPostcode = record.get("OrgPostcode");
+
+		return address;
 	}
 	
 	private  CSVAddress() {
@@ -33,6 +47,8 @@ public class CSVAddress {
 
 	private String psuedoPersonId;
 	private String orgPostcode;
+
+
 
 	public String getQ() {
 		return line1 + " " + line2 + " " + line3 + " " + line4 + " " + county + " " + postcode;
